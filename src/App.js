a468309ace902c105e57ead59e1c713f10607cc2
Nastasja Jovicic <jovicic.nastasja@gmail.com>
2018-12-10 23:52:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    let targetUrl = 'https://mysticalraven.com/wp-json/wp/v2/posts/';
+    return fetch(proxyUrl + targetUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        posts: data
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+           {this.state.posts.map((item, index) => (
+           <div key={index}>
+             <div className="row">
+               <div className="leftcolumn">
+                 <div className="card">
+                   <div className= "title">
+                     <h1>{item.title.rendered}</h1>
+                   </div>
+                   <div className= "content" dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
+                 </div>
+               </div>
+             </div>
+           </div>
+           ))}
+     </div>
       </div>
     );
   }
